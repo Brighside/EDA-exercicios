29d4fe92f26497b7aeb3246b27d604ca5f581e6f
brighside <2000fellipe@gmail.com>
2020-10-20 11:57:17 -0300

    Imprementação do BubbleSort

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -17,7 +17,7 @@ void bubbleSort (int *array) {
     int aux = 0;
     for(int i = 0; i < 4; i++) { //compara um elemento do array com todos os outros
         for(int j = i + 1; j < 5; j++) {
-            if(array[i] > array[j]) {
+            if(array[i] > array[j]) { // para ordenar em ordem decrescente, trocar ">" por "<"
                 aux = array[i];
                 array[i] = array[j];
                 array[j] = aux;
